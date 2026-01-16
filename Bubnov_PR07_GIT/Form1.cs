ccc8a1eba48c1042a0a0d044ecd78c4397a2e5a4
Bubnov <nekit.b.nb@mail.ru>
2026-01-16 07:42:05 +0300

    Исправлен переход на форму администратора

--- Bubnov_PR07_GIT/Form1.cs
+++ Bubnov_PR07_GIT/Form1.cs
@@ -16,5 +16,12 @@ namespace Bubnov_PR07_GIT
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+            this.Hide();
+        }
     }
 }
